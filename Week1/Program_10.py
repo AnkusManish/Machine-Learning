50bd02787618026278581f6bab002409aa2c959c
AnkusManish <ankusmanish@gmail.com>
2019-07-29 16:37:15 +0530

    Print elements present in list_1 but not in list_2

---
+++ Week1/Program_10.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 27 12:43:15 2019
+
+@author: ankusmanish
+"""
+
+color_list_1 = set(["White", "Black", "Red"]) 
+color_list_2 = set(["Red", "Green"])
+#storing values of color_list_1 which are not present in color_list_2
+list3 = [a for a in color_list_1 if a not in color_list_2]    
+print(set(list3))
