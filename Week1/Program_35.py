36c5f74e804477ed11e84ae367db8a3676d7c081
AnkusManish <ankusmanish@gmail.com>
2019-07-29 17:02:57 +0530

    Printing 15 multiples with any anonymous function

---
+++ Week1/Program_35.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 27 14:31:27 2019
+
+@author: ankusmanish
+"""
+
+def func():
+    for i in range(1,11):
+        print(15 * i)
+        
+#function that multiplies the numbers between 1-10        
+func()        
+
+#Another way of getting the multiples of 15
+
+"""
+def func1():
+    for i in range(1,200):
+        if (i % 15 == 0):
+            print(i)
+
+"""
