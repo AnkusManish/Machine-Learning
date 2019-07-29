b89ab48ae98a8f688b6c7a593cc6d3b09e7d3fa4
AnkusManish <ankusmanish@gmail.com>
2019-07-29 16:57:36 +0530

    Printing the host name

---
+++ Week1/Program_29.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 27 13:42:49 2019
+
+@author: ankusmanish
+"""
+
+import socket
+
+#returns the hostname
+print(socket.gethostname())
