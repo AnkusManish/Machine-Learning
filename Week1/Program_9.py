cfb5ad97df0b40ead663bcef4855f51ddca609eb
AnkusManish <ankusmanish@gmail.com>
2019-07-30 11:09:50 +0530

    Concatenating all elements in a list into a string and return it

--- Week1/Program_9.py
+++ Week1/Program_9.py
@@ -6,9 +6,9 @@ Created on Sat Jul 27 12:41:02 2019
 @author: ankusmanish
 """
 #taking input(int) from the user and splitting it with a comma
-given_list = [int(x) for x in input('Enter the numbers').split(',')]
+given_list = [int(x) for x in input('Enter the numbers ').split(',')]
 lis = []
 #looping the given list, converting the numbers to string and storing them in the lis variable
 for i in given_list:
     lis.append(str(i))
-print(lis)  
+print('The given string is {}'.format(' '.join(lis)))  
