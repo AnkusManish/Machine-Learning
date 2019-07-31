9327b123f3601e55916a98ccba48495fb5bca6c5
AnkusManish <ankusmanish@gmail.com>
2019-07-31 22:44:50 +0530

    Creating a set

--- Week2/Sets/Program_1.py
+++ Week2/Sets/Program_1.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 22:39:32 2019
 
+@author: ankusmanish
+"""
+
+#creating a set
+a = set([1,2,3,4,5])
+print(a)
+
+#second way of creating sets
+b = {1,2,3,4,5}
+print(b)
