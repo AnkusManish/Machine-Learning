b6f15834fb004c4b620f1009904ab04ab116506e
AnkusManish <ankusmanish@gmail.com>
2019-07-31 20:58:46 +0530

    Iterating over dictionaries using loops

---
+++ Week2/Dictionary/Program_4.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 20:54:05 2019
+
+@author: ankusmanish
+"""
+
+d = {'val_1':8,'val_2':21,'val_3':7,'val_4':20,'val_5':47,'val_6':53,'val_7':29,'val_8':88,'val_9':30,}
+
+#unpacking keys and values from the dictionary
+for k,v in d.items():
+    print('Key :',k, 'Value :',v)
+    
