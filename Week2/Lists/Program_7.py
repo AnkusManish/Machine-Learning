2606642cbd2d5d910ecdf93753afebf184d90a75
AnkusManish <ankusmanish@gmail.com>
2019-08-01 00:04:37 +0530

    Cloning the list

---
+++ Week2/Lists/Program_7.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 00:01:57 2019
+
+@author: ankusmanish
+"""
+
+a = [1,2,3,4,5,4,5]
+
+#copy() method copies the list from one variable
+b = a.copy()
+
+print(b)
