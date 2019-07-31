35b3447670db98f0fe91e3317f27058de3543d0f
AnkusManish <ankusmanish@gmail.com>
2019-07-31 23:29:13 +0530

    Creating frozenset

---
+++ Week2/Sets/Program_11.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 23:26:42 2019
+
+@author: ankusmanish
+"""
+
+#frozenset() returns a type of set which is immutable
+a = frozenset([1,2,3,4,5])
+
+print(a)
