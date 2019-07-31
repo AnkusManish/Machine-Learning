ec990a6ba876ad231a4b892e0be5e9ccf2cf300b
AnkusManish <ankusmanish@gmail.com>
2019-07-31 23:31:14 +0530

    Finding min and max from a set

---
+++ Week2/Sets/Program_12.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 23:29:18 2019
+
+@author: ankusmanish
+"""
+
+a = {37,7,33,68,29,42,89}
+
+#max() returns the maximum value from the set
+#min() returns the minimum value from the set
+print('The minimum value in the set is : {}'.format(min(a)))
+print("THe maximum value in the list is : {}".format(max(a)))
