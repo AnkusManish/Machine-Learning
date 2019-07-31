a4bddf5eeab1ac9af39cd50485ba1b7a7aed5d6b
AnkusManish <ankusmanish@gmail.com>
2019-07-31 23:26:30 +0530

    Clear a set

---
+++ Week2/Sets/Program_10.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 23:24:38 2019
+
+@author: ankusmanish
+"""
+
+a = set([1,2,3,4,5])
+
+#clear() method clears the set and makes it empty
+a.clear()
+
+print(a)
