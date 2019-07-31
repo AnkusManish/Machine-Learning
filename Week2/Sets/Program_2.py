236d1d195e774d38b43a94f37131b006f4eeb1d1
AnkusManish <ankusmanish@gmail.com>
2019-07-31 22:49:40 +0530

    Iterating over set

---
+++ Week2/Sets/Program_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 22:43:57 2019
+
+@author: ankusmanish
+"""
+
+a = set([1,2,3,4,5])
+
+#iterating over set
+for i in a:
+    print(i)
