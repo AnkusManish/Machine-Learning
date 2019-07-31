9d545540dcdb63fbe46f798b89a5c2dbeb485169
AnkusManish <ankusmanish@gmail.com>
2019-07-31 23:12:17 +0530

    Creating intersection of sets

---
+++ Week2/Sets/Program_6.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 23:08:29 2019
+
+@author: ankusmanish
+"""
+
+a = set([1,2,3,4,5])
+b = set([2,4,7,8,5])
+
+#intersection() method returns the values present in seta and also setb
+res = a.intersection(b)
+
+print(res)
