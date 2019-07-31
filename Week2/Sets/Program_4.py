097069f16edca49321583041b8a63011802a8804
AnkusManish <ankusmanish@gmail.com>
2019-07-31 22:54:33 +0530

    Removing item from the set

---
+++ Week2/Sets/Program_4.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 22:52:10 2019
+
+@author: ankusmanish
+"""
+
+a = set([1,2,3,4,5])
+
+#remove() method removes item from the set
+a.remove(5)
+
+print('Updated set is : {}'.format(a))
