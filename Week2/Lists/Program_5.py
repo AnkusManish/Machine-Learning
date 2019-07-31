666bfb48c86a478990259231232ae20a30ff456b
AnkusManish <ankusmanish@gmail.com>
2019-07-31 23:56:15 +0530

    sort tuple based on the last value in a tuple

---
+++ Week2/Lists/Program_5.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 23:50:39 2019
+
+@author: ankusmanish
+"""
+
+a = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+
+#sorting elements based on the last element in the tuple
+print(sorted(a, key = lambda x : x[-1]))
