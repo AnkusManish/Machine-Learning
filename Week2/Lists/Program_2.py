26b43c6083a1c9210d37f33ac40247c21dd58f89
AnkusManish <ankusmanish@gmail.com>
2019-07-31 23:38:18 +0530

    Multiply all the items in the list

---
+++ Week2/Lists/Program_2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 23:35:34 2019
+
+@author: ankusmanish
+"""
+
+a = [1,2,3,4,5]
+
+b = 1
+#loop that iterates through list and mutiplies 
+for i in a:
+    b = b * i
+    
+print(b)    
