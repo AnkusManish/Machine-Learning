800564975ae973cc6583e4e99f33563a5f5b11b9
AnkusManish <ankusmanish@gmail.com>
2019-07-31 20:52:08 +0530

    Adding key to a dictionary

---
+++ Week2/Dictionary/Program_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 20:48:19 2019
+
+@author: ankusmanish
+"""
+
+d = {'val_1':8,'val_2':21,'val_3':7,'val_4':20,'val_5':47,'val_6':53,'val_7':29,'val_8':88,'val_9':30,}
+
+#update() method adds the key-value to a dictionary
+d.update(val_10 = 37)
+
+print(d)
+
