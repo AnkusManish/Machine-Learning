55476d75a63267882a6db1b2144d928038f65275
AnkusManish <ankusmanish@gmail.com>
2019-07-31 22:52:03 +0530

    Adding element to a set

---
+++ Week2/Sets/Program_3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 22:49:49 2019
+
+@author: ankusmanish
+"""
+
+a = set([1,2,3,4,5])
+
+#add() method adds the element to the set
+a.add(6)
+
+print('Updated list is : {}'.format(a))
