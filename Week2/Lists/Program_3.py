9d0f5d6aa12e840ed5905a6add84f7e88a2249b3
AnkusManish <ankusmanish@gmail.com>
2019-07-31 23:40:25 +0530

    Finding smallest element in the list

---
+++ Week2/Lists/Program_3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 23:38:27 2019
+
+@author: ankusmanish
+"""
+
+a = [37,7,33,68,29,42,89]
+
+#min() returns minimum value from the list
+print(min(a))
