153561a004f6bfcdca9c03075eb16b5df82e4218
AnkusManish <ankusmanish@gmail.com>
2019-07-31 23:34:58 +0530

    Sum of items in the list

--- Week2/Lists/Program_1.py
+++ Week2/Lists/Program_1.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 23:32:55 2019
 
+@author: ankusmanish
+"""
+
+a = [1,2,3,4,5]
+
+#sum() adds all the values in the list
+print(sum(a))
