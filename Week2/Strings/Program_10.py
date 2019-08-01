1ae7bc4d4159c5828365da2a82804c00afdb7698
AnkusManish <ankusmanish@gmail.com>
2019-08-01 11:05:14 +0530

    Retruning the count of a substring in the string

---
+++ Week2/Strings/Program_10.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 11:03:31 2019
+
+@author: ankusmanish
+"""
+
+string = '''
+  The body was found floating near Hoige Bazar at around 6.30am. The next process will go as per law and 
+  family members have been informed," Mangaluru police commissioner (city) Sandeep Patil said.
+  Nethravathi river is located on the outskirts of Mangaluru - 350km from Bengaluru and very close to the Arabian Sea.
+  '''
+
+#returns the count of the given substring in the string
+print(string.count('the'))
