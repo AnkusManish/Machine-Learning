2beb70c911daf4d24fa15b1c795cc6cda307b63e
AnkusManish <ankusmanish@gmail.com>
2019-08-01 10:02:47 +0530

    Splitting a list based on its first character

---
+++ Week2/Lists/Program_16.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 10:00:35 2019
+
+@author: ankusmanish
+"""
+
+word_list = ['be','have','do','say','get','make','go','know','take','see','come','think',
+     'look','want','give','use','find','tell','ask','work','seem','feel','leave','call']
+
+#sorted() arranges the string in the alphabetical order
+print(sorted(word_list))
