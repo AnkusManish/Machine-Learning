34eaabb5a04be91cd5585d1fd79a0fa3c3b7a84c
AnkusManish <ankusmanish@gmail.com>
2019-08-01 10:32:14 +0530

    Slicing the values from a tuple

---
+++ Week2/Tuple/Program_9.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 10:28:55 2019
+
+@author: ankusmanish
+"""
+
+a = (1,2,3,4,5)
+
+
+#Slicing the values from a tuple
+print(a[0:4])
+
+print(a[::2])
