687073df4de7c9d014a59f2a7a83e4598c57c432
AnkusManish <ankusmanish@gmail.com>
2019-08-01 10:14:20 +0530

    Creating a tuple with different data types

---
+++ Week2/Tuple/Program_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 10:12:12 2019
+
+@author: ankusmanish
+"""
+
+a = ('Manish','Anish',1,3,6)
+
+#variable that holds different data types as a tuple
+
+print(a)
