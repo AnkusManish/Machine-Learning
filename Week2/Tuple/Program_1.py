3c68d99a628283e902a5acbcc1ed8bf779d2ec90
AnkusManish <ankusmanish@gmail.com>
2019-08-01 10:11:35 +0530

    Creating a tuple

--- Week2/Tuple/Program_1.py
+++ Week2/Tuple/Program_1.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 10:09:27 2019
 
+@author: ankusmanish
+"""
+
+a = (1,2,3,4)
+#two ways to create tuples
+b = tuple([5,6,7])
+
+print(a)
+print(b)
