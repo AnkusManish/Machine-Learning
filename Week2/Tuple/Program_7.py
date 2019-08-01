97735ea25b03ff3565a569621008e7c513497664
AnkusManish <ankusmanish@gmail.com>
2019-08-01 10:25:50 +0530

    Converting tuple to a list

---
+++ Week2/Tuple/Program_7.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 10:23:25 2019
+
+@author: ankusmanish
+"""
+
+a = (1,2,3,4,4,5,29,56,3,5,2,86)
+
+print('Tuple : {}'.format(a))
+#Converting tuple to a list
+print('Tuple convered to list : {}'.format(list(a)))
+
