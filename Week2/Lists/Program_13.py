e72bced3bc275a73ff7a906529ca9b2db1958bad
AnkusManish <ankusmanish@gmail.com>
2019-08-01 09:55:25 +0530

    Combining two lists into one

---
+++ Week2/Lists/Program_13.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 09:52:58 2019
+
+@author: ankusmanish
+"""
+
+a = [1,2,3,4,5,2]
+b = [6,7,8,9,0,1]
+
+#Combining two lists with '+' operator
+print(list(a) + list(b))
