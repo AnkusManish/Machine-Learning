ee789dfe0243609eea9a82918cd363fff6798870
AnkusManish <ankusmanish@gmail.com>
2019-08-01 09:50:30 +0530

    Generating permutations of a given list

---
+++ Week2/Lists/Program_11.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 09:48:34 2019
+
+@author: ankusmanish
+"""
+
+import itertools
+
+#permutation() method returns the possibilities of ordering the given set of numbers
+print(list(itertools.permutations([1,2,3,4])))
