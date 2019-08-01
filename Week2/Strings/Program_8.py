cf03ac8fa7354c46d5bdee62b6aee0d70f404c22
AnkusManish <ankusmanish@gmail.com>
2019-08-01 11:00:47 +0530

    Splitting the string

---
+++ Week2/Strings/Program_8.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 10:56:57 2019
+
+@author: ankusmanish
+"""
+
+string = 'https://www.w3resource.com/python-exercises'
+
+#splitting the string with the specified delimiter
+res = string.split('-')[0]
+
+print(res)
