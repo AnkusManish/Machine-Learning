231567e7bafa261b1c2765ecb5748546c0caa9e7
AnkusManish <ankusmanish@gmail.com>
2019-08-01 10:37:51 +0530

    Get the length of the given string

--- Week2/Strings/Program_1.py
+++ Week2/Strings/Program_1.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 10:35:24 2019
 
+@author: ankusmanish
+"""
+
+a = 'Manish'
+
+#prints the length of the string
+print(len(a))
