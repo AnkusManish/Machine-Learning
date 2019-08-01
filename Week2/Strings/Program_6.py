0b12f6921dc0531a0ac0c2b26abc2d0a5d6b4c6e
AnkusManish <ankusmanish@gmail.com>
2019-08-01 10:53:02 +0530

    Converting a string to lower and upper case

---
+++ Week2/Strings/Program_6.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 10:51:22 2019
+
+@author: ankusmanish
+"""
+
+string = input('Enter any string ')
+
+#Upper() converts the string to uppercase
+print('Given string in Upper case : {}'.format(string.upper()))
+
+#Lower() converts the string to lowercase
+print('Given string in Lower case : {}'.format(string.lower()))
