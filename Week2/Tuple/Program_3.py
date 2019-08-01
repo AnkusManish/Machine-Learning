a707ad1fe228ddd5e15519e4b9425e87c2582f51
AnkusManish <ankusmanish@gmail.com>
2019-08-01 10:15:50 +0530

    Unpacking the values of tuple a into different variables

---
+++ Week2/Tuple/Program_3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 10:14:26 2019
+
+@author: ankusmanish
+"""
+
+a = ('Manish','Anish',1,3,6)
+
+#unpacking the values of tuple a into different variables
+(v,w,x,y,z) = a
+
+print(v)
+print(w)
+print(x)
+print(y)
+print(z)
