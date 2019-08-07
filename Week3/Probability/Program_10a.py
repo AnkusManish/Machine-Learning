0da3ea54340ed46f889350bbb3ffcffe2d89f151
AnkusManish <ankusmanish@gmail.com>
2019-08-07 18:49:57 +0530

    P(x < 40)

---
+++ Week3/Probability/Program_10a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  7 16:26:36 2019
+
+@author: ankusmanish
+"""
+
+print('Given conditions : ')
+print('\n')
+µ = 30
+σ = 4
+x = 40
+print()
+print('z = (x - µ) /σ')
+print(' ','= (40 - 30) / 4')
+result = (x - µ) / σ
+print(' ','= ',result)
+print()
+#The area to the left of 2.5
+#z(2.5) = 0.9938
+print('P(z < 2.5) = ',0.9938)
