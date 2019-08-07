3e14a842ff1c083998ec09d530415552c9d150d5
AnkusManish <ankusmanish@gmail.com>
2019-08-07 18:47:01 +0530

    P(90<Y<110)

---
+++ Week3/Probability/Program_7.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  7 16:23:55 2019
+
+@author: ankusmanish
+"""
+
+print('Given conditions : ')
+print('\n')
+print('Number of customers the banker serves : {}'.format(50))
+print('Mean : {}'.format(2))
+print('Variance : {}'.format(1))
+print()
+print('P(90<Y<=110) = ø(√2) - ø(-√2)')
+print()
+result = 0.9207 - 0.0793
+print('P(90<Y<=110) : {}'.format(result))
